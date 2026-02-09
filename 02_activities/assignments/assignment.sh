043e80fa3041deb39e29f21a737863e59ff1e2cf
Simon Meng <simon.meng97@gmail.com>
2026-02-09 12:32:32 -0500

    answered number 5

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -41,6 +41,7 @@ ls ./data/raw
 mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cp ./newproject/data/raw/*server*.log ./newproject/data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
 
